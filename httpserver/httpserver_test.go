56f450453adb06be208c12efa32f88beefb83f35
Bera <brunormoura@gmail.com>
2018-07-27 06:53:38 -0300

    [Refactored] dried up http test file

--- httpserver/httpserver_test.go
+++ httpserver/httpserver_test.go
@@ -1,6 +1,7 @@
 package httpserver
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -8,29 +9,30 @@ import (
 
 func TestGETItems(t *testing.T) {
 	t.Run("returns XPTO's score", func(t *testing.T) {
-		request, _ := http.NewRequest(http.MethodGet, "/items/XPTO", nil)
+		request := newGetScoreRequest("XPTO")
 		response := httptest.NewRecorder()
+
 		ItemServer(response, request)
-		got := response.Body.String()
-		want := "20"
-		if got != want {
-			t.Errorf("got '%s', want '%s'", got, want)
-		}
+		assertResponseBody(t, response.Body.String(), "20")
 	})
 
 	t.Run("returns XPTO2's score", func(t *testing.T) {
-		request, _ := http.NewRequest(http.MethodGet, "/items/XPTO2", nil)
+		request := newGetScoreRequest("XPTO")
 		response := httptest.NewRecorder()
 
 		ItemServer(response, request)
-
-		got := response.Body.String()
-		want := "10"
-
-		if got != want {
-			t.Errorf("got '%s', want '%s'", got, want)
-		}
-
+		assertResponseBody(t, response.Body.String(), "20")
 	})
+}
 
+func newGetScoreRequest(name string) *http.Request {
+	req, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/items/%s", name), nil)
+	return req
+}
+
+func assertResponseBody(t *testing.T, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("response body is wrong, got '%s' want '%s'", got, want)
+	}
 }
